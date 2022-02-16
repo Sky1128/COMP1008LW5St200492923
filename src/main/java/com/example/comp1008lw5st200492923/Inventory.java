2bcd70b34080afa4734cc98d9e186aa0269d6e51
sky1128 <pa.pruthvish@gmail.com>
2022-02-15 23:37:02 -0500

    Array List

---
+++ src/main/java/com/example/comp1008lw5st200492923/Inventory.java
@@ -0,0 +1,13 @@
+package com.example.comp1008lw5st200492923;
+
+import java.util.ArrayList;
+
+public class Inventory {
+
+    ArrayList<Phone> phones;
+
+    public Inventory (){
+        phones = new ArrayList<Phone>();
+    }
+
+}
