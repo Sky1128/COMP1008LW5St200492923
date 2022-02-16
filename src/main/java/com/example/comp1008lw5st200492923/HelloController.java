27609e80944034b8022ce9aa3efd44ec590a0ba5
sky1128 <pa.pruthvish@gmail.com>
2022-02-15 23:29:37 -0500

    Initial commit

---
+++ src/main/java/com/example/comp1008lw5st200492923/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.comp1008lw5st200492923;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
